2be7c21ade86695e2308cc7c9a45919e01fd3f35
carlaal44 <carlafernandezalvarez4@gmail.com>
2026-04-23 13:31:49 +0200

    First Commit

---
+++ src/main/java/com/example/arc/Mentiroso/MentirosoApplication.java
@@ -0,0 +1,13 @@
+package com.example.arc.Mentiroso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentirosoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentirosoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/arc/Mentiroso/MentirosoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.arc.Mentiroso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentirosoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
